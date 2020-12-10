258c72cbbfe937acf2986c25a858283f73fe8077
prabaharan ramachandran <prabaharan.ramachandran@rrd.com>
2020-12-11 03:00:35 +0530

    insert into iable

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '34.66.185.168',
-  user     : 'prabaei',
+  user     : 'praba',
   password : '123456',
   database : 'StudyPlan'
 });
  
 connection.connect();
  
-connection.query(`CREATE TABLE MyDictionary
-(
-   Id int NOT NULL AUTO_INCREMENT,
-   Word nvarchar(200) not null,
-   Meaning nvarchar(800) not null,
-TamilEquivalent nvarchar(200) null
-   PRIMARY KEY(Id)
-);`, function (error, results, fields) {
+connection.query(`insert into MyDictionary(Word,Meaning,TamilEquivalent) values('Product-1','example','wow');`,
+ function (error, results, fields) {
+  console.log(results);
+  console.log(fields);
+
   if (error) throw error;
   console.log('The solution is: done');
 });
